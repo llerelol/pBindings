efb4f23f9323154282db62f2c95a206deeabcc34
llerelol <petr.komogortsev@gmail.com>
2012-12-13 17:11:42 +1100

    warlocks basic binds

--- bindings/warlock.lua
+++ bindings/warlock.lua
@@ -1,196 +1,118 @@
 ﻿if(select(2, UnitClass'player') ~= 'WARLOCK') then return end
 
-local _, bindings = ...
-
-local affliction = {
-	's|Drain Life',
-	's|Drain Soul',
-	's|Unstable Affliction',
-	's|Corruption',
-	's|Life Tap',
-	's|Haunt',
-
-	['`'] = 'm|/use [mod:alt]Demonic Circle: Summon;[nomod:alt]Demonic Circle: Teleport',
-
-	A = 's|Shadowflame',
-	Z = 's|Soul Harvest',
-	P = 's|Death Coil',
-	W = 's|Fear',
-	X = 's|Shoot',
-	Q = 's|Shadowburn',
-	G = 's|Soulshatter',
-	V = 's|Demon Soul',
-	D = 'm|/target Obnoxious Fiend',
-
-	F1 = 's|Summon Imp',
-	F2 = 's|Summon Voidwalker',
-	F3 = 's|Summon Succubus',
-	F4 = 's|Summon Felhunter',
-
-	F7 = 's|Fel Armor',
-	F8 = 's|Dark Intent',
-
-	F11 = 's|Ritual of Summoning',
-	F12 = 's|Enslave Demon',
-
-	BUTTON4 = 'm|/petattack',
-	BUTTON5 = 'm|/swapactionbar 1 2',
-
+local __, bindings = ...
+local lockbase = {
+	
+	C = 's|Демонический круг: телепортация',
+	X = 's|Демонический круг: создание',
+	G = 'm|/cast Темное восстановление\n/cast Жатва жизни',
+	T = 's|Жизнеотвод',
+	V = 'm|',
+	B = 's|',
+	R = 'm|/cast Неистовство тьмы\n/cast Лик тлена\n/cast Вой ужаса',
+	F1 = 's|Призыв беса',
+	F2 = 's|Призыв охотника Скверны',
+	F3 = 's|Призыв суккуба',
+	F4 = 's|Призыв демона Бездны',
+	F5 = 's|Призыв стража Скверны',
+	TAB = 's|Твердая решимость',
+	BUTTON3 = 's|Каждый за себя',
+	BUTTON4 = 's|Доспех скверны',
+	BUTTON5 = 's|Узы Тьмы',
+	--'m|/cast Гримуар жертвоприношения\n/cast Гримуар служения\n/cast Гримуар верховной власти',
+	-- naga
+	NUMPAD1 = 's|',
+	END = 'm|',
+	-- NUMPAD2 spec specific
+	NUMPAD3 = 'm|/focus [@mouseover,exists,nodead][]',
+	PAGEDOWN = 's|',
+	
+	NUMPAD4 = 's|',
+	LEFT = 'm|',
+	F6 = 's|', -- num5
+	NUMPAD6 = 's|',
+	RIGHT = 'm|',
+	NUMPAD7 = 'm|/cast Белый боевой конь-скелет\n/cast Боевой скакун грозовой вершины',
+	NUMPAD8 = 's|',
+	NUMPAD9 = 's|',
+	NUMPADPLUS = 'm|/use 15',
+	['`'] = 's|',
+	['¸'] = 's|',
+	
 	shift = {
-		's|Immolate',
-		's|Corruption',
-		's|Shadow Bolt',
-
-		V = 's|Shadow Ward',
+		's|Нестабильное колдовство',
+		's|Стрела Тьмы',
+		's|Порча',
+		's|Похищение жизни',
+		's|Семя порчи',
+		
+		G = 'm|',
+		E = 'm|',
+		F = 'm|',
+		Q = 's|Страх',
+		C = 's|',
+		T = 's|Углеотвод',
+		
+		MOUSEWHEELUP = 'm|',
+		MOUSEWHEELDOWN = 'm|',
+		SPACE = 'm|/use 6',
+		F6 = 'm|', --num5
 	},
-
-	alt = {
-		's|Seed of Corruption',
-		's|Shadowfury',
-	},
-
 	ctrl = {
-		C = 's|Howl of Terror'
+		's|',
+		's|',
+		's|',
+		's|',
+		Q = 'm|/cast [@focus,exists,harm,nodead] Страх',
+		E = 's|',
+		R = 'i|Камень здоровья',
+		T = 'm|/cast [@mouseover,help,nodead][help,nodead][@player] Камень души',
+		F = 'm|',
+		NUMPAD2 = 's|',
+		NUMPAD4 = 'm|',
+		F6 = 'm|', -- num5
+		NUMPAD6 = 'm|',
 	},
-
-	bar2 = {
-		's|Bane of Agony',
-		's|Bane of Doom',
-		's|Bane of Havoc',
-		's|Curse of the Elements',
-		's|Curse of Tongues',
+	alt = {
+		'm|/petattack',
+		'm|/petfollow',
+		E = 'm|/cast Темная сделка\n/cast Жертвенный договор',
+		Q = 's|Завеса сумерек',
+		R = 's|Создание камня здоровья',
 	},
 }
 
-local destruction = {
-	's|Incinerate',
-	's|Conflagrate',
-	's|Chaos Bolt',
-	's|Soul Fire',
-	's|Life Tap',
+local affli = {
+	's|',
+	's|',
+	's|',
+	's|',
+	's|',
+}
 
-	['`'] = 'm|/use [mod:alt]Demonic Circle: Summon;[nomod:alt]Demonic Circle: Teleport',
+local demo = {
+	's|',
+	's|',
+	's|',
+	's|',
+	's|',
+}
 
-	A = 's|Shadowflame',
-	Z = 's|Soul Harvest',
-	P = 's|Death Coil',
-	W = 's|Fear',
-	X = 's|Shoot',
-	Q = 's|Shadowburn',
-	G = 's|Soulshatter',
-	V = 's|Demon Soul',
-	D = 'm|/target Obnoxious Fiend',
-
-	F1 = 's|Summon Imp',
-	F2 = 's|Summon Voidwalker',
-	F3 = 's|Summon Succubus',
-	F4 = 's|Summon Felhunter',
-
-	F7 = 's|Fel Armor',
-	F8 = 's|Dark Intent',
-
-	F11 = 's|Ritual of Summoning',
-	F12 = 's|Enslave Demon',
-
-	BUTTON4 = 'm|/petattack',
-	BUTTON5 = 'm|/swapactionbar 1 2',
-
+local destro = {
+	's|Пламя Скверны',
+	's|Жертвенный огонь',
+	's|Испепеление',
+	's|Стрела Хаоса',
+	's|Хаос',
+	E = 's|Поджигание',
+	Q = 'm|/cast !Огненный ливень',
+	F = 's|Ожог Тьмы',
+	
 	shift = {
-		's|Immolate',
-		's|Corruption',
-
-		V = 's|Shadow Ward',
-	},
-
-	alt = {
-		's|Seed of Corruption',
-		's|Shadowfury',
-	},
-
-	ctrl = {
-		C = 's|Howl of Terror'
-	},
-
-	bar2 = {
-		's|Bane of Agony',
-		's|Bane of Doom',
-		's|Bane of Havoc',
-		's|Curse of the Elements',
-		's|Curse of Tongues',
+		R = 's|Огонь и сера',
 	},
 }
 
-local demonology = {
-	's|Incinerate',
-	"s|Hand of Gul'dan",
-	's|Shadow Bolt',
-	's|Soul Fire',
-	's|Life Tap',
-
-	['`'] = 'm|/use [mod:alt]Demonic Circle: Summon;[nomod:alt]Demonic Circle: Teleport',
-
-	A = 's|Shadowflame',
-	Z = 's|Soul Harvest',
-	P = 's|Death Coil',
-	W = 's|Fear',
-	X = 's|Shoot',
-	Q = 's|Shadowburn',
-	G = 's|Soulshatter',
-	V = 's|Demon Soul',
-	D = 'm|/target Obnoxious Fiend',
-
-	F1 = 's|Summon Imp',
-	F2 = 's|Summon Voidwalker',
-	F3 = 's|Summon Succubus',
-	F4 = 's|Summon Felhunter',
-	F5 = 's|Summon Felguard',
-
-	F7 = 's|Fel Armor',
-	F8 = 's|Dark Intent',
-
-	F11 = 's|Ritual of Summoning',
-	F12 = 's|Enslave Demon',
-
-	BUTTON4 = 'm|/petattack',
-	BUTTON5 = 'm|/swapactionbar 1 2',
-
-	shift = {
-		's|Immolate',
-		's|Corruption',
-		"s|Hand of Gul'dan",
-		"s|Immolation Aura",
-
-		V = 's|Metamorphosis',
-	},
-
-	alt = {
-		's|Seed of Corruption',
-		's|Shadowfury',
-	},
-
-	ctrl = {
-		C = 's|Howl of Terror'
-	},
-
-	bar2 = {
-		's|Bane of Agony',
-		's|Bane of Doom',
-		's|Bane of Havoc',
-		's|Curse of the Elements',
-		's|Curse of Tongues',
-	},
-}
-
-oBindings:RegisterCallback(function(self, state)
-	if(oPanel and oPanel.fade) then
-		if(state == 'bar2') then
-			oPanel.fade:SetGradientAlpha("VERTICAL", .1, .1, .1, 0, .25, .35, .25, 1)
-		elseif(state == 'base') then
-			oPanel.fade:SetGradientAlpha("VERTICAL", .1, .1, .1, 0, .25, .25, .35, 1)
-		end
-	end
-end)
-
-oBindings:RegisterKeyBindings('Affliction', bindings.base, affliction)
-oBindings:RegisterKeyBindings('Destruction', bindings.base, destruction)
-oBindings:RegisterKeyBindings('Demonology', bindings.base, demonology)
+pBindings:RegisterKeyBindings('Колдовство', bindings.base, lockbase, affli)
+pBindings:RegisterKeyBindings('Демонология', bindings.base, lockbase, demo)
+pBindings:RegisterKeyBindings('Разрушение', bindings.base, lockbase, destro)
